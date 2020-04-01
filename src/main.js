f3148fec05f80284cc538fba5ec31eb018e76c23
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-01 10:36:38 +0300

    Creates function generate markup sort

--- src/main.js
+++ src/main.js
@@ -28,3 +28,13 @@ const createNavigationComponent = () => {
     </nav>`
   );
 };
+
+const createSortComponent = () => {
+  return (
+    `<ul class="sort">
+      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" class="sort__button">Sort by date</a></li>
+      <li><a href="#" class="sort__button">Sort by rating</a></li>
+    </ul>`
+  );
+};
