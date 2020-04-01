a3eac2743b3ee7a5453730029c0820b21f38082e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-01 10:33:59 +0300

    Adds films card amount

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@
 const pageHeaderElement = document.querySelector(`.header`);
 const pageMainElement = document.querySelector(`.main`);
 const pageFooterElement = document.querySelector(`.footer`);
+const filmCards = [0, 1, 2, 3, 4];
+const filmTopRatedCards = [0, 1];
+const filmMostCommentedCards = [0, 1];
