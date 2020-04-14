f1d256c9a3ad8c2b3c13a5813c3604d0d84cddb6
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-14 08:53:13 +0300

    Generates random values

--- src/mock/comments.js
+++ src/mock/comments.js
@@ -4,10 +4,10 @@ import {TEXT_COMMENTS, EMOJIS, COMMENT_AUTHORS, COMMENT_DATES} from './constants
 
 const generateComment = () => {
   return {
-    text: `Interesting setting and a good cast`,
-    emoji: `./images/emoji/smile.png`,
-    author: `Tim Macoveev`,
-    date: `2019/12/31 23:59`,
+    text: getRandomArrayItem(TEXT_COMMENTS),
+    emoji: getRandomArrayItem(EMOJIS),
+    author: getRandomArrayItem(COMMENT_AUTHORS),
+    date: getRandomArrayItem(COMMENT_DATES),
   };
 };
 
