2b976077dabef6747b970e05b4b92ee9c51f4f4d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-14 07:15:56 +0300

    Creates func get random int number

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 export const renderComponent = (container, component, place = `beforeend`) => {
   container.insertAdjacentHTML(place, component);
 };
+
+const getRandomIntegerNumber = (min, max) => {
+  return min + Math.floor(Math.random() * (max - min));
+};
