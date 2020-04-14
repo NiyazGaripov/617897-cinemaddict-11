155dd38e1a0e7daf67d7740966a7570d2ccaab3c
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-14 07:21:13 +0300

    Creates func get random array item and exports functions form module

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,15 @@
-export const renderComponent = (container, component, place = `beforeend`) => {
+const renderComponent = (container, component, place = `beforeend`) => {
   container.insertAdjacentHTML(place, component);
 };
 
 const getRandomIntegerNumber = (min, max) => {
   return min + Math.floor(Math.random() * (max - min));
 };
+
+const getRandomArrayItem = (array) => {
+  const randomItem = getRandomIntegerNumber(0, array.length);
+
+  return array[randomItem];
+};
+
+export {renderComponent, getRandomIntegerNumber, getRandomArrayItem};
