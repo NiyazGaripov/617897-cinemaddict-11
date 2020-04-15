d908ee5389e48eb6c9011dc3ff82ad93b80bcb32
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-15 21:29:00 +0300

    Imports modules and fills film card mock data

--- src/mock/film-cards.js
+++ src/mock/film-cards.js
@@ -1,20 +1,33 @@
-import {generateComments} from './comments.js';
 import {getRandomIntegerNumber, getRandomArrayItem} from './../utils.js';
+import {POSTERS, FILM_TITLES, DURATION, GENRE, DESCRIPTION_FILMS} from './constants.js';
+import {generateComments} from './comments.js';
 
+const DATE = new Date();
+const MIN_RATING = 0;
+const MAX_RATING = 9;
+const MIN_RELEASE_YEAR = 1920;
+const MAX_RELEASE_YEAR = DATE.getFullYear();
 const MIN_COMMENTS_AMOUNT = 0;
 const MAX_COMMENTS_AMOUNT = 5;
+const poster = getRandomArrayItem(POSTERS);
+const title = getRandomArrayItem(FILM_TITLES);
+const rating = `${getRandomIntegerNumber(MIN_RATING, MAX_RATING)}.${getRandomIntegerNumber(MIN_RATING, MAX_RATING)}`;
+const releaseYear = getRandomIntegerNumber(MIN_RELEASE_YEAR, MAX_RELEASE_YEAR);
+const duration = getRandomArrayItem(DURATION);
+const genre = getRandomArrayItem(GENRE);
+const description = getRandomArrayItem(DESCRIPTION_FILMS);
 const commentsAmount = getRandomIntegerNumber(MIN_COMMENTS_AMOUNT, MAX_COMMENTS_AMOUNT);
 const comments = generateComments(commentsAmount);
 
 const generateFilmCard = () => {
   return {
-    poster: `./images/posters/the-dance-of-life.jpg`,
-    name: `The Dance of Life`,
-    rating: `8.3`,
-    releaseYear: `1929`,
-    duration: `1h 55m`,
-    genre: `Musical`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    poster,
+    title,
+    rating,
+    releaseYear,
+    duration,
+    genre,
+    description,
     comments,
   };
 };
