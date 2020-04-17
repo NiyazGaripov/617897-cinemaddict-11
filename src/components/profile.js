bcef94357aef6c125d1bc2cc578ede2b0570748a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-17 14:04:40 +0300

    Imports dependences and adds random user rank

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,8 +1,14 @@
-export const createProfileComponent = () => {
+import {PROFILE} from './../mock/constants.js';
+import {getRandomArrayItem} from './../utils.js';
+
+const createProfileComponent = () => {
+  const userRank = getRandomArrayItem(PROFILE);
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
+      <p class="profile__rating">${userRank}</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`
   );
 };
+
+export {createProfileComponent};
