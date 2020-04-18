3ae2292c4665920ed150dae70e6a7f3528c65f0c
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-18 19:02:06 +0300

    Creates func generate sort list

--- src/mock/sort-list.js
+++ src/mock/sort-list.js
@@ -1,1 +1,11 @@
 import {SORT_ITEM_NAMES} from './constants.js';
+
+const generateSortList = () => {
+  return SORT_ITEM_NAMES.map((it) => {
+    return {
+      title: it,
+    };
+  });
+};
+
+export {generateSortList};
