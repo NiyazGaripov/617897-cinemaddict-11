1ad63a2c8e0d54ac22f66d8ad65435087f9bb66f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-18 16:28:43 +0300

    Creates nav-list module and func it

---
+++ src/mock/nav-list.js
@@ -0,0 +1,15 @@
+import {NAVIGATION_ITEM_NAMES} from './constants.js';
+
+const generateNavigationList = () => {
+  const firstElement = 0;
+  const coefficient = 10;
+  return NAVIGATION_ITEM_NAMES.map((it) => {
+    return {
+      path: it.split(` `)[firstElement].toLowerCase(),
+      title: it,
+      amount: Math.floor(Math.random() * coefficient),
+    };
+  });
+};
+
+export {generateNavigationList};
