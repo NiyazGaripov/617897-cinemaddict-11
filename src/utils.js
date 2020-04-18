62747235d6140cc8b10b538177f0488dfd42f6e9
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-18 20:28:07 +0300

    Creates func get random date

--- src/utils.js
+++ src/utils.js
@@ -12,4 +12,15 @@ const getRandomArrayItem = (array) => {
   return array[randomItem];
 };
 
-export {renderComponent, getRandomIntegerNumber, getRandomArrayItem};
+const getRandomDate = () => {
+  const MIN_COEFFICIENT = 0;
+  const MAX_COEFFICIENT = 30000;
+  const targetDate = new Date();
+  const diffValue = getRandomIntegerNumber(MIN_COEFFICIENT, MAX_COEFFICIENT);
+
+  targetDate.setDate(targetDate.getDate() - diffValue);
+
+  return targetDate;
+};
+
+export {renderComponent, getRandomIntegerNumber, getRandomArrayItem, getRandomDate};
