a9202bc760e78a4d1ccc432d493edf4fd3b4c67b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-19 16:06:18 +0300

    Imports dependences

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import {renderComponent} from './utils.js';
 import {createProfileComponent} from './components/profile.js';
 import {createNavigationComponent} from './components/navigation.js';
 import {createSortComponent} from './components/sorting.js';
+import {createFilmCardsComponent} from './components/film-cards.js';
 import {createFilmsListComponent} from './components/film-list.js';
+import {createShowMoreButtonComponent} from './components/show-more-button.js';
 import {createFilmStatisticsComponent} from './components/film-statistics.js';
 import {createFilmDetailsComponent} from './components/film-details.js';
 import {generateNavigationList} from './mock/nav-list.js';
 import {generateSortList} from './mock/sort-list.js';
+import {generateFilmsCards} from './mock/film-cards.js';
 import {FILM_SECTIONS} from './mock/constants.js';
 
 const pageHeader = document.querySelector(`.header`);
