095c4dd99507a995a0356cfa1c10e936c8da6a69
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-19 16:05:40 +0300

    Changes names constants and adds film sections

--- src/main.js
+++ src/main.js
@@ -7,16 +7,17 @@ import {createFilmStatisticsComponent} from './components/film-statistics.js';
 import {createFilmDetailsComponent} from './components/film-details.js';
 import {generateNavigationList} from './mock/nav-list.js';
 import {generateSortList} from './mock/sort-list.js';
+import {FILM_SECTIONS} from './mock/constants.js';
 
-const pageHeaderElement = document.querySelector(`.header`);
-const pageMainElement = document.querySelector(`.main`);
-const pageFooterElement = document.querySelector(`.footer`);
+const pageHeader = document.querySelector(`.header`);
+const pageMain = document.querySelector(`.main`);
+const pageFooter = document.querySelector(`.footer`);
 const navList = generateNavigationList();
 const sortList = generateSortList();
 
-renderComponent(pageHeaderElement, createProfileComponent());
-renderComponent(pageMainElement, createNavigationComponent(navList));
-renderComponent(pageMainElement, createSortComponent(sortList));
-renderComponent(pageMainElement, createFilmsListComponent());
-renderComponent(pageFooterElement, createFilmStatisticsComponent());
-renderComponent(document.body, createFilmDetailsComponent());
+renderComponent(pageHeader, createProfileComponent());
+renderComponent(pageMain, createNavigationComponent(navList));
+renderComponent(pageMain, createSortComponent(sortList));
+renderComponent(pageMain, createFilmsListComponent(FILM_SECTIONS));
+renderComponent(pageFooter, createFilmStatisticsComponent());
+// renderComponent(document.body, createFilmDetailsComponent(filmCards[0]));
