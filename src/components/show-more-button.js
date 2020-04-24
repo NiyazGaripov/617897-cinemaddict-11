519333afee78427f2430a286b8920068d501a064
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-04-25 00:02:25 +0300

    Imports create element and creates class show more button

--- src/components/show-more-button.js
+++ src/components/show-more-button.js
@@ -1,7 +1,31 @@
+import {createElement} from './../utils.js';
+
 const createShowMoreButtonComponent = () => {
   return (
     `<button class="films-list__show-more">Show more</button>`
   );
 };
 
-export {createShowMoreButtonComponent};
+class ShowMoreButton {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createShowMoreButtonComponent();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export {ShowMoreButton};
