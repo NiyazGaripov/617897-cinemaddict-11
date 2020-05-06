e4304875cd93498b6c9659963419b5d08c20e04e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-06 20:38:49 +0300

    Refactor methods PageController

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -12,6 +12,7 @@ const BEGIN_INDEX = 0;
 const renderFilmCard = (filmsListContainer, filmCard) => {
   const body = document.body;
   const filmCardComponent = new FilmCard(filmCard);
+  const filmInfoComponent = new FilmInfo(filmCard);
 
   const showFilmDetails = () => {
     body.classList.add(`hide-overflow`);
@@ -31,71 +32,52 @@ const renderFilmCard = (filmsListContainer, filmCard) => {
   };
 
   filmCardComponent.setClickHandler(showFilmDetails);
-
-  const filmInfoComponent = new FilmInfo(filmCard);
-
   filmInfoComponent.setClickHandler(hideFilmDetails);
 
   renderComponent(filmsListContainer, filmCardComponent);
 };
 
-const renderFilmList = (filmListComponent, films) => {
-  const filmsListSection = filmListComponent.getElement().querySelector(`.films-list`);
-  const filmsListContainer = filmListComponent.getElement().querySelector(`.films-list .films-list__container`);
-  const hasFilms = films.length > 0;
-
-  if (!hasFilms) {
-    renderComponent(filmsListSection, new NoData());
-  }
-
-  let showingFilmCards = FILM_CARDS_AMOUNT_ON_START;
-
-  const renderCards = (cards, container, begin, end) => {
-    cards
-    .slice(begin, end)
-    .forEach((card) => {
-      renderFilmCard(container, card);
-    });
-  };
-
-  renderCards(films, filmsListContainer, BEGIN_INDEX, showingFilmCards);
-
-  const showMoreButtonComponent = new ShowMoreButton();
-
-  renderComponent(filmsListSection, showMoreButtonComponent);
-
-  showMoreButtonComponent.setClickHandler(() => {
-    const prevFilmCards = showingFilmCards;
-    showingFilmCards += FILM_CARDS_AMOUNT_LOAD_MORE;
-
-    renderCards(films, filmsListContainer, prevFilmCards, showingFilmCards);
-
-    if (showingFilmCards >= films.length) {
-      removeComponent(showMoreButtonComponent);
-    }
+const renderFilmCards = (cards, container) => {
+  cards.forEach((card) => {
+    renderFilmCard(container, card);
   });
-};
-
-const renderFilmListExtra = (filmListComponent, films, domElement) => {
-  const filmsListContainer = filmListComponent.getElement().querySelector(domElement);
-
-  films
-    .forEach((card) => {
-      renderFilmCard(filmsListContainer, card);
-    });
 };
 
 class PageController {
   constructor(container) {
     this._container = container;
+    this._noData = new NoData();
+    this._showMoreButton = new ShowMoreButton();
   }
 
   renderFilms(films) {
-    renderFilmList(this._container, films);
+    const filmsListSection = this._container.getElement().querySelector(`.films-list`);
+    const filmsListContainer = this._container.getElement().querySelector(`.films-list .films-list__container`);
+    const hasFilms = films.length > 0;
+    let showingFilmCards = FILM_CARDS_AMOUNT_ON_START;
+
+    if (!hasFilms) {
+      renderComponent(filmsListSection, this._noData);
+    }
+
+    renderFilmCards(films.slice(BEGIN_INDEX, showingFilmCards), filmsListContainer);
+    renderComponent(filmsListSection, this._showMoreButton);
+
+    this._showMoreButton.setClickHandler(() => {
+      const prevFilmCards = showingFilmCards;
+      showingFilmCards += FILM_CARDS_AMOUNT_LOAD_MORE;
+
+      renderFilmCards(films.slice(prevFilmCards, showingFilmCards), filmsListContainer);
+
+      if (showingFilmCards >= films.length) {
+        removeComponent(this._showMoreButton);
+      }
+    });
   }
 
   renderFilmsExtra(films, className) {
-    renderFilmListExtra(this._container, films, className);
+    const filmsListContainer = this._container.getElement().querySelector(className);
+    renderFilmCards(films, filmsListContainer);
   }
 }
 
