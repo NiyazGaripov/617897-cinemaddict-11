46ec71c1c9ef502c7c82cc972995ec4b67dc956a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-16 20:38:08 +0300

    Creates func createEmojiItemComponent

--- src/components/emoji-list.js
+++ src/components/emoji-list.js
@@ -1,1 +1,10 @@
 import {AbstractComponent} from './../components/abstract-component.js';
+
+const createEmojiItemComponent = (emoji) => {
+  return (
+    `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}">
+    <label class="film-details__emoji-label" for="emoji-${emoji}">
+      <img src="./images/emoji/${emoji}.png" width="30" height="30" alt="emoji-${emoji}">
+    </label>`
+  );
+};
