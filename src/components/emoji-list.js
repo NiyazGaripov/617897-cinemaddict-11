105b38226c78510efaa8d01608b75b93d6bae329
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-16 20:52:48 +0300

    Refactors func

--- src/components/emoji-list.js
+++ src/components/emoji-list.js
@@ -1,10 +1,11 @@
 import {AbstractComponent} from './../components/abstract-component.js';
+import {EMOJIS} from './../mock/constants.js';
 
-const createEmojiItemComponent = (emoji) => {
+const createEmojiListComponent = (currentEmoji) => EMOJIS.map((emoji) => {
   return (
-    `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}">
+    `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}" ${currentEmoji ? `checked` : ``}>
     <label class="film-details__emoji-label" for="emoji-${emoji}">
       <img src="./images/emoji/${emoji}.png" width="30" height="30" alt="emoji-${emoji}">
     </label>`
   );
-};
+});
