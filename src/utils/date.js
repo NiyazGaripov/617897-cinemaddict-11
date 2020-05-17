24d328d724725e174d8ce6efc73fba1a0e419012
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-17 12:39:25 +0300

    Creates module date and transfers functions from module common

---
+++ src/utils/date.js
@@ -0,0 +1,20 @@
+import moment from 'moment';
+import {getRandomIntegerNumber} from './common.js';
+
+const MIN_COEFFICIENT = 0;
+const MAX_COEFFICIENT = 30000;
+
+const getRandomDate = () => {
+  const targetDate = new Date();
+  const diffValue = getRandomIntegerNumber(MIN_COEFFICIENT, MAX_COEFFICIENT);
+
+  targetDate.setDate(targetDate.getDate() - diffValue);
+
+  return targetDate;
+};
+
+const getFullYear = (date) => {
+  return moment(date).format(`YYYY`);
+};
+
+export {getRandomDate, getFullYear};
