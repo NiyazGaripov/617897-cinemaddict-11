81841d27f90f1db043b69158cae241dea88a5a81
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-19 16:46:21 +0300

    Creates class FilterController and adds constructor

--- src/controllers/filter.js
+++ src/controllers/filter.js
@@ -2,3 +2,20 @@ import {Filter} from '../components/filter.js';
 import {FilterType, FILTER_NAMES} from "../mock/constants.js";
 import {renderComponent, replaceComponent} from "../utils/render.js";
 import {getFilmsByFilter} from "../utils/filter.js";
+
+class FilterController {
+  constructor(container, filmsModel) {
+    this._container = container;
+    this._filmsModel = filmsModel;
+
+    this._activeFilterType = FilterType.ALL;
+    this._filterComponent = null;
+
+    this._onDataChange = this._onDataChange.bind(this);
+    this._onFilterChange = this._onFilterChange.bind(this);
+
+    this._filmsModel.setDataChangeHandler(this._onDataChange);
+  }
+}
+
+export {FilterController};
