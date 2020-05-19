fdfac42f8836c43e32e02d2636ed3fa956dc4c10
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-19 16:31:59 +0300

    Imports dependences

---
+++ src/controllers/filter.js
@@ -0,0 +1,4 @@
+import {Filter} from '../components/filter.js';
+import {FilterType, FILTER_NAMES} from "../mock/constants.js";
+import {renderComponent, replaceComponent} from "../utils/render.js";
+import {getFilmsByFilter} from "../utils/filter.js";
