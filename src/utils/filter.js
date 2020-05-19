52553ce7d084865a5da0707a5800232d027a46be
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-19 20:31:08 +0300

    Imports filter type and adds switch to func

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,5 +1,18 @@
-const getFilmsByFilter = (films) => {
+import {FilterType} from './../mock/constants.js';
+
+const getFilmsByFilter = (films, filterType) => {
+  switch (filterType) {
+    case FilterType.WATCHLIST:
+      return getWatchListFilms(films);
+    case FilterType.HISTORY:
+      return getWatchedFilms(films);
+    case FilterType.FAVORITES:
+      return getFavoritesFilms(films);
+  }
+
   return films;
 };
 
 export {getFilmsByFilter};
+
+
