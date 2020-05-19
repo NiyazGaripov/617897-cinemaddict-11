eaf793bb40242cd8045b4160adc2ffc500a41493
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-19 17:53:33 +0300

    Creates method render

--- src/controllers/filter.js
+++ src/controllers/filter.js
@@ -1,6 +1,6 @@
 import {Filter} from '../components/filter.js';
 import {FilterType, FILTER_NAMES} from "../mock/constants.js";
-import {renderComponent, replaceComponent} from "../utils/render.js";
+import {RenderPosition, renderComponent, replaceComponent} from "../utils/render.js";
 import {getFilmsByFilter} from "../utils/filter.js";
 
 class FilterController {
@@ -15,6 +15,31 @@ class FilterController {
     this._onFilterChange = this._onFilterChange.bind(this);
 
     this._filmsModel.setDataChangeHandler(this._onDataChange);
+  }
+
+  render() {
+    const container = this._container;
+    const allFilms = this._filmsModel.getFilms();
+
+    const filters = Object.values(FilterType).map((filterType, index) => {
+      return {
+        path: filterType,
+        title: FILTER_NAMES[index],
+        amount: filterType !== FilterType.ALL ? getFilmsByFilter(allFilms, filterType).length : null,
+        isActive: filterType === this._activeFilterType,
+      };
+    });
+
+    const oldComponent = this._filterComponent;
+
+    this._filterComponent = new Filter(filters);
+    this._filterComponent.setFilterClickHandler(this._onFilterChange);
+
+    if (oldComponent) {
+      replaceComponent(this._filterComponent, oldComponent);
+    } else {
+      renderComponent(container, this._filterComponent, RenderPosition.AFTERBEGIN);
+    }
   }
 }
 
