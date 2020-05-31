3d2291eb567be547a3f4af89a41dae90fda033e3
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-05-31 11:39:31 +0300

    Makes refactor date utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -25,13 +25,18 @@ const formatCommentDate = (date) => {
   return moment(date).format(`YYYY/MM/DD HH:mm`);
 };
 
-const getFilmDuration = (duration) => {
+const getFilmDuration = (duration, isStatistic = false) => {
   const filmDuration = moment.duration(duration, `minutes`);
   let hours = filmDuration.hours();
   let minutes = filmDuration.minutes();
 
-  hours = hours > 0 ? `${hours}h` : ``;
-  minutes = minutes > 0 ? `${minutes}m` : ``;
+  if (isStatistic) {
+    hours = hours > 0 ? `${hours}<span class="statistic__item-description">h</span>` : ``;
+    minutes = minutes > 0 ? `${minutes}<span class="statistic__item-description">m</span>` : ``;
+  } else {
+    hours = hours > 0 ? `${hours}h` : ``;
+    minutes = minutes > 0 ? `${minutes}m` : ``;
+  }
 
   if (hours && minutes) {
     hours += ` `;
