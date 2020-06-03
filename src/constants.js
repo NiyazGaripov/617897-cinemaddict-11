c4515adb4c12aa09edac1730cfb4d3def3a73708
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-06-03 10:47:03 +0300

    Removes unused constants

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,13 @@
-const TEXT_COMMENTS = [
-  `Interesting setting and a good cast`,
-  `Booooooooooring`,
-  `Very very old. Meh`,
-  `Almost two hours? Seriously?`
-];
+const FILM_CARDS_AMOUNT_ON_START = 5;
+const FILM_CARDS_AMOUNT_LOAD_MORE = 5;
+const FILM_RATED_CARDS_AMOUNT = 2;
+const FILM_COMMENTED_CARDS_AMOUNT = 2;
+const BEGIN_INDEX = 0;
+
+const Mode = {
+  DEFAULT: `default`,
+  MODAL: `modal`,
+};
 
 const EMOJIS = [
   `smile`,
@@ -12,93 +16,7 @@ const EMOJIS = [
   `angry`,
 ];
 
-const COMMENT_AUTHORS = [
-  `Tim Macoveev`,
-  `John Doe`,
-  `Tim Doe`,
-  `John Macoveev`,
-];
-
-const POSTERS = [
-  `the-dance-of-life.jpg`,
-  `sagebrush-trail.jpg`,
-  `the-man-with-the-golden-arm.jpg`,
-  `santa-claus-conquers-the-martians.jpg`,
-  `popeye-meets-sinbad.png`,
-  `the-great-flamarion.jpg`,
-  `made-for-each-other.png`,
-];
-
-const FILM_TITLES = [
-  `The Dance of Life`,
-  `Sagebrush Trail`,
-  `The Man with the Golden Arm`,
-  `Santa Claus Conquers the Martians`,
-  `Popeye the Sailor Meets Sindbad the Sailor`,
-  `The Great Flamarion`,
-  `Made for Each Other`,
-];
-
-const GENRES = [
-  `Musical`,
-  `Western`,
-  `Drama`,
-  `Comedy`,
-  `Cartoon`,
-  `Mystery`,
-];
-
-const DESCRIPTION_FILMS = [
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
-  `Fusce tristique felis at fermentum pharetra.`,
-  `Aliquam id orci ut lectus varius viverra.`,
-  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
-  `Aliquam erat volutpat.`,
-  `Nunc fermentum tortor ac porta dapibus.`,
-  `In rutrum ac purus sit amet tempus.`,
-];
-
-const AGES = [
-  `6+`,
-  `12+`,
-  `18+`
-];
-
-const DIRECTORS = [
-  `Anthony Mann`,
-  `Steven Spielberg`,
-  `Martin Scorsese`,
-  `Ridley Scott`,
-  `John Woo`,
-  `Christopher Nolan`,
-  `Peter Jackson`,
-];
-
-const WRITERS = [
-  `Anne Wigton`,
-  `Heinz Herald`,
-  `Richard Weil`,
-];
-
-const ACTORS = [
-  `Erich von Stroheim`,
-  `Mary Beth Hughes`,
-  `Dan Duryea`,
-];
-
-const COUNTRIES = [
-  `USA`,
-  `Russia`,
-  `England`,
-  `Norway`,
-  `Canada`,
-];
-
-const FilmSection = {
+const filmSection = {
   all: {
     section: `all`,
     title: `All movies. Upcoming`
@@ -119,6 +37,12 @@ const UserRank = {
   MOVIE_BUFF: `Movie Buff`,
 };
 
+const RangValue = {
+  MIN: 1,
+  MEDIUM: 10,
+  MAX: 20
+};
+
 const FILTER_NAMES = [
   `All movies`,
   `Watchlist`,
@@ -133,12 +57,6 @@ const FilterType = {
   FAVORITES: `favorites`,
 };
 
-const SORT_ITEM_NAMES = [
-  `default`,
-  `date`,
-  `rating`,
-];
-
 const SortType = {
   DEFAULT: `default`,
   DATE: `date`,
@@ -151,14 +69,6 @@ const ESC_KEYCODE = 27;
 const HIDDEN_CLASS = `visually-hidden`;
 
 const BAR_HEIGHT = 50;
-
-const PERIOD_NAMES = [
-  `All time`,
-  `Today`,
-  `Week`,
-  `Month`,
-  `Year`,
-];
 
 const PeriodFilterType = {
   ALL_TIME: `all-time`,
@@ -179,9 +89,16 @@ const Method = {
 
 const ENTRY_POINT = `https://11.ecmascript.pages.academy/cinemaddict`;
 
+const HttpStatus = {
+  SUCCESS: 200,
+  REDIRECT: 300
+};
+
 const NavigationType = {
   FILTER: `filters`,
   STATS: `stats`,
 };
 
-export {TEXT_COMMENTS, EMOJIS, COMMENT_AUTHORS, POSTERS, FILM_TITLES, FilterType, GENRES, DESCRIPTION_FILMS, AGES, DIRECTORS, WRITERS, ACTORS, COUNTRIES, FilmSection, UserRank, FILTER_NAMES, SORT_ITEM_NAMES, SortType, ESC_KEYCODE, HIDDEN_CLASS, BAR_HEIGHT, PERIOD_NAMES, PeriodFilterType, AUTHORIZATION, Method, ENTRY_POINT, NavigationType};
+const ANIMATION_TIMEOUT = 600;
+
+export {FILM_CARDS_AMOUNT_ON_START, FILM_CARDS_AMOUNT_LOAD_MORE, FILM_RATED_CARDS_AMOUNT, FILM_COMMENTED_CARDS_AMOUNT, BEGIN_INDEX, Mode, EMOJIS, FilterType, filmSection, UserRank, RangValue, FILTER_NAMES, SortType, ESC_KEYCODE, HIDDEN_CLASS, BAR_HEIGHT, PeriodFilterType, AUTHORIZATION, Method, ENTRY_POINT, HttpStatus, NavigationType, ANIMATION_TIMEOUT};
